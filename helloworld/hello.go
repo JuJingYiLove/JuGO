1f63ed81f67732050f6ba085d1e4280279810ee3
JuYingYiLove <qiansu.love@gmail.com>
2023-12-25 19:49:00 +0800

    update

---
+++ helloworld/hello.go
@@ -0,0 +1,10 @@
+package helloworld
+import (
+	"fmt"
+)
+var printf = fmt.Printf
+var scanf = fmt.Scanf
+
+func Hello(){
+	printf("helloworld")
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"JuGo/helloworld"
 )
 var printf = fmt.Printf
 var scanf = fmt.Scanf
@@ -10,5 +11,6 @@ func main()  {
 	var __new int 
 	scanf("%d",&__new)
 	printf("hello %d",__new)
-	
+	helloworld.Hello()
+
 }
